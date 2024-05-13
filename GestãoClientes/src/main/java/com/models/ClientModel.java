b43ef7b0d0ed89042082054fec2a843c9c07b596
Mariane-Medeiros <mariane.oli.med@gmail.com>
2024-05-13 15:23:46 -0300

    Model update

--- GestãoClientes/src/main/java/com/models/ClientModel.java
+++ GestãoClientes/src/main/java/com/models/ClientModel.java
@@ -25,4 +25,59 @@ public class ClientModel implements Serializable{
 	
 	@Column
 	private String telefone;
+	
+	@Column
+	private String nomeUsuario;
+	
+	@Column
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
