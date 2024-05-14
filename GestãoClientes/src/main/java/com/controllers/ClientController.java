a284fd002a91cfcfc1877d3569736123e577dd0c
Mariane-Medeiros <mariane.oli.med@gmail.com>
2024-05-14 15:39:52 -0300

    Controller

--- GestãoClientes/src/main/java/com/controllers/ClientController.java
+++ GestãoClientes/src/main/java/com/controllers/ClientController.java
@@ -5,11 +5,15 @@ import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dtos.ClientDTO;
@@ -19,44 +23,34 @@ import com.services.ClientServices;
 
 @RestController
 public class ClientController {
-	
-	//ta faltando botar um @responsebody em alguns mas nao sei pra que serve
-	
+
 	@Autowired
-	ClientRepository clientRepository;
-	
 	ClientServices clientservice;
 	
 	@PostMapping("/clients")
-		private void createClient() {
-			clientservice.createCliente(ClientModel client);
-	}
-	
-	@GetMapping("/clients")
-	private List<ClientModel> getAllClient() { //fazer ele retornar um reponseentity(ver qual é melhor)
-		List<ClientModel> clients = clientRepository.findAll();
-		//isempty
-		return clients;	
+		public ResponseEntity createClient(@RequestBody ClientDTO clientDto) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(clientservice.saveClient(clientDto));
 		}
 	
+	@GetMapping("/clients")
+		public ResponseEntity getAllClients() {
+		return ResponseEntity.status(HttpStatus.OK).body(clientservice.getAllClient());
+		}
+		
+	
 	@GetMapping("/clients/{id}")
-	private Optional<ClientModel> getOneClient(@PathVariable(value="id") long id) { //fazer ele retornar um reponseentity(ver qual é melhor)
-		Optional<ClientModel> client = clientRepository.findById(id);
-		//isempty
-		return client;
+	public ResponseEntity getOneClient(@PathVariable(value = "id")long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(clientservice.getOneClient(id));
 	}
 	
 	@PutMapping("/clients/{id}")
-	private void putClient(@PathVariable(value="id") long id, ClientDTO dtoClient) {
-		Optional<ClientModel> entityClient = clientRepository.findById(id);
-		//validao isempty
-		//aparentemento eu devia colocar esse entity em um model?? nn entendi porque
-		BeanUtils.copyProperties(dtoClient, entityClient);
+	public ResponseEntity putClient(@PathVariable(value = "id")long id, @RequestBody ClientDTO clientDto) {
+		return ResponseEntity.status(HttpStatus.OK).body(clientservice.updateClient(clientDto, id));
 	}
 	
-	@DeleteMapping("/client/{id}")
-	private void deleteClient(@PathVariable(value="id") long id) {
-		clientRepository.deleteById(id); //a entidade nao pode ser null para a função retornar, entao talvez tenha que verificar esse tambem
+	@DeleteMapping("/clients/{id}")
+	public ResponseEntity deleteClient(@PathVariable(value = "id")long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(clientservice.deleteClient(id));
 	}
 	
 	
